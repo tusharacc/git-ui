f21df3276be381fd7138aa1414a160804443df6a
Tushar Saurabh <tusharsaurabh@Tushars-Mac-mini.local>
2021-04-18 13:36:01 -0400

    Updated Git Version logic

--- src/renderer.js
+++ src/renderer.js
@@ -1,6 +1,16 @@
 const {ipcRenderer} = require('electron')
+const { callExecFile } = require('./common/cli')
 
 
 function raiseEvent(channel,...args){
     ipcRenderer.send(channel,args)
 }
+
+let gitversion = document.getElementById('gitversion')
+
+callExecFile('git','--version')
+.then( (data)=> {
+    gitversion.innerText = data;
+    console.log("The output is", data)
+})
+.catch(err => console.log("The error log is",err))
